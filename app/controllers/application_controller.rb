6ae9f280db0273c1c903c4f62bdbc00e4f30b1ca
yoshi522 <yoshihito522@gmail.com>
2016-09-17 11:01:43 +0900

    added filter ssl expire

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :expire_hsts
 
 
   def hello
     render text:'hello from yoshi'
+  end
+
+
+  private
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
   end
 end
